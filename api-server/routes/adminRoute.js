1e5869f8a31ad782b618fb628364c6ff88caa4c7
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2025-02-24 18:54:18 +0530

    feat: added check availability route

--- api-server/routes/adminRoute.js
+++ api-server/routes/adminRoute.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { addDoctor , getAllDoctors, loginAdmin } from "../controller/adminController.js";
+import {
+  addDoctor,
+  getAllDoctors,
+  loginAdmin,
+} from "../controller/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
+import { changeAvailablity } from "../controller/doctorController.js";
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
-adminRouter.post('/login',loginAdmin);
-adminRouter.post('/get-all-doctors',authAdmin,getAllDoctors)
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRouter.post("/login", loginAdmin);
+adminRouter.post("/get-all-doctors", authAdmin, getAllDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailablity);
 
 export default adminRouter;
