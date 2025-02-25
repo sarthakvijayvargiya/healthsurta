fe5874aee6d0a1761e8177a80b2b6457981d4cd5
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2025-02-25 17:37:24 +0530

    fix: user login api

--- api-server/routes/userRoute.js
+++ api-server/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../controller/userController.js";
+import { loginUser, registerUser } from "../controller/userController.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register-user",registerUser)
+userRoute.post("/login",loginUser)
 
 export default userRoute; 
