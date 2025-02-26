94a5dfe51228547dab3d5f27cab3d7be7524cdb4
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2025-02-26 14:50:07 +0530

    feat: created update profile api to update user data

--- api-server/controller/userController.js
+++ api-server/controller/userController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
 // API- to register user
 const registerUser = async (req, res) => {
   try {
@@ -52,15 +53,14 @@ const loginUser = async (req, res) => {
       return res.json({ success: false, message: "User does not exist" });
     }
 
-    const isMatch = await bcrypt.compare(password,user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    if(isMatch){
-      const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-      res.json({success:true,token})
-    }else{
-      res.json({success:false,message:"Invalid Credentials"})
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
     }
-
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
@@ -68,18 +68,52 @@ const loginUser = async (req, res) => {
 };
 
 // API to get user profile data
-const getProfile = async (req,res) => {
- try {
-  // user will send token , will get userId from that and send that using req.body
-  const {userId} = req.body;
-  const userData = await userModel.findById(userId).select('-password');
+const getProfile = async (req, res) => {
+  try {
+    // user will send token , will get userId from that and send that using req.body
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId).select("-password");
 
-  res.json({success:true,userData});
-
- } catch (error) {
+    res.json({ success: true, userData });
+  } catch (error) {
     console.log(error);
-    res.json({success:false,message:error.message});
- }  
-}
+    res.json({ success: false, message: error.message });
+  }
+};
 
-export { registerUser,loginUser,getProfile };
+// API to update user profile
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, address, dob, gender, phone } = req.body;
+    const imageFile = req.file;
+
+    if (!name || !phone || !dob || !gender) {
+      res.json({ success: false, message: "Data Missing" });
+    }
+
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    });
+
+    if (imageFile) {
+      // upload image to cloudinary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageUrl = imageUpload.secure_url;
+
+      await userModel.findByIdAndUpdate(userId, { image: imageUrl });
+    }
+
+    res.json({ success: true, message: "Profile Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getProfile, updateProfile };
--- api-server/routes/userRoute.js
+++ api-server/routes/userRoute.js
@@ -1,11 +1,15 @@
 import express from "express"
-import { getProfile, loginUser, registerUser } from "../controller/userController.js";
+import { getProfile, loginUser, registerUser, updateProfile } from "../controller/userController.js";
 import authUser from "../middleware/authUser.js";
+import upload from "../middleware/multer.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register-user",registerUser)
 userRoute.post("/login",loginUser)
 userRoute.get("/get-profile",authUser,getProfile)
+
+// TODO Check this api flow to understand
+userRoute.post("/update-profile", upload.single('image') ,authUser,updateProfile)
 
 export default userRoute; 
